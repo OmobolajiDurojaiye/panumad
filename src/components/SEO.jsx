dc0571b36f279f4839865a2ebe56cee3dc23c1e7
Omobolaji Durojaiye <99079690+OmobolajiDurojaiye@users.noreply.github.com>
2026-04-02 08:30:19 +0100

    seo

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,7 +1,10 @@
 // src/components/SEO.jsx
 import { Helmet } from 'react-helmet-async';
 
-const SEO = ({ title, description, name, type }) => {
+const SEO = ({ title, description, name, type, image }) => {
+const defaultImage = "https://www.panumad.com/og-image.png";
+const seoImage = image || defaultImage;
+
 return (
 <Helmet>
     { /* Standard metadata tags */ }
@@ -11,15 +14,17 @@ return (
     
     { /* End standard metadata tags */ }
     { /* Facebook tags */ }
-    <meta property="og:type" content={type} />
+    <meta property="og:type" content={type || 'website'} />
     <meta property="og:title" content={title} />
     <meta property="og:description" content={description} />
+    <meta property="og:image" content={seoImage} />
     { /* End Facebook tags */ }
     { /* Twitter tags */ }
     <meta name="twitter:creator" content={name} />
-    <meta name="twitter:card" content={type} />
+    <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content={title} />
     <meta name="twitter:description" content={description} />
+    <meta name="twitter:image" content={seoImage} />
     { /* End Twitter tags */ }
 </Helmet>
 )
