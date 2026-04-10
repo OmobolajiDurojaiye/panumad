e734a3ae0001fdb3a6a9a19e67cc4dab4cade7a2
Omobolaji Durojaiye <99079690+OmobolajiDurojaiye@users.noreply.github.com>
2026-04-10 13:46:26 +0100

    sen

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+const Prerender = require('vite-plugin-prerender')
+const PuppeteerRenderer = require('@prerenderer/renderer-puppeteer')
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Prerender({
+      staticDir: path.join(__dirname, 'dist'),
+      routes: ['/', '/about', '/projects', '/gallery', '/contact'],
+      renderer: new PuppeteerRenderer({
+        headless: true,
+        executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'
+      })
+    }),
+  ],
 })
